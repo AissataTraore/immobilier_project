fbf446aeedddb21314c5dee00f8ceddea3d2155d
MohamedMDiop <mougou.diop@gmail.com>
2025-01-21 20:38:09 +0000

    prediction complete

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
 from flask import Flask, render_template, request, jsonify, redirect, url_for, flash
 import pymysql
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 
@@ -10,9 +11,11 @@ app.config['MYSQL_USER'] = 'root'  # Utilisateur MySQL
 app.config['MYSQL_PASSWORD'] = ''  # Mot de passe MySQL
 app.config['MYSQL_DB'] = 'customers_base'  # Base de données MySQL
 # mysql = MySQL(app)
-
+app.config['SECRET_KEY'] = 'your_secret_key' 
 # Initialisation de l'extension MySQL
 #mysql = MySQL(app)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:@localhost/customers_base'
+#db = SQLAlchemy(app)
 
 methods=['POST', 'PUT', 'GET', 'PATCH', 'DELETE']
 
@@ -78,6 +81,83 @@ def delete_customer(id):
     cur.execute("DELETE FROM client WHERE id = %s", (id))
     cur.connection.commit()
     return redirect(url_for('list_customers'))
+
+#from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, BooleanField, SubmitField, RadioField
+import pickle
+import os
+model_path = os.path.join(os.path.dirname(__file__), 'model', 'model.pkl')
+# Charger le modèle
+with open(model_path, 'rb') as f:
+    lm = pickle.load(f)
+
+class PredictionForm(FlaskForm):
+    area = IntegerField('Superficie')
+    bedrooms = IntegerField('Chambres')
+    bathrooms = IntegerField('Salles de bain')
+    stories = IntegerField('Étages')
+    mainroad = BooleanField('Accès à la route principale')
+    guestroom = BooleanField('Chambre d\'amis')
+    basement = BooleanField('Sous-sol')
+    hotwaterheating = BooleanField('Chauffage à eau chaude')
+    airconditioning = BooleanField('Climatisation')
+    parking = IntegerField('Places de parking')
+    prefarea = BooleanField('Quartier privilégié')
+    # RadioField avec les bonnes valeurs pour semi-furnished et unfurnished
+    semi_furnished = RadioField(
+        'Meubles',
+        choices=[('00', 'Meublé'), ('01', 'Non-meublé'), ('10', 'Semi-meublé')],
+        default='01'  # Valeur par défaut "Non-meublé"
+    )
+
+    submit = SubmitField('Prédire')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    form = PredictionForm()
+    if form.validate_on_submit():
+        # Récupérer les données du formulaire
+        data = [
+            int(form.area.data),
+            int(form.bedrooms.data),
+            int(form.bathrooms.data),
+            int(form.stories.data),
+            int(form.mainroad.data),  # Convertir Boolean en entier (0 ou 1)
+            int(form.guestroom.data),
+            int(form.basement.data),
+            int(form.hotwaterheating.data),
+            int(form.airconditioning.data),
+            int(form.parking.data),
+            int(form.prefarea.data)
+        ]
+        
+        # Récupérer la valeur du champ semi_furnished
+        semi_furnished_value = form.semi_furnished.data
+        
+        # Décomposer en semi_furnished_value et unfurnished_value
+        if semi_furnished_value == '10':
+            # Semi-meublé : '10' -> semi_furnished_value = 1, unfurnished_value = 0
+            data.append(1)  # Semi-meublé -> semi_furnished_value = 1
+            data.append(0)  # Non-meublé -> unfurnished_value = 0
+        elif semi_furnished_value == '01':
+            # Non-meublé : '01' -> semi_furnished_value = 0, unfurnished_value = 1
+            data.append(0)  # Non-meublé -> semi_furnished_value = 0
+            data.append(1)  # Non-meublé -> unfurnished_value = 1
+        else:
+            # Meublé : '00' -> semi_furnished_value = 0, unfurnished_value = 0
+            data.append(0)  # Meublé -> semi_furnished_value = 0
+            data.append(0)  # Meublé -> unfurnished_value = 0
+
+        # Préparer les données pour le modèle
+        data = [data]  # Mettre les données dans le format attendu par votre modèle
+        
+        # Faire la prédiction
+        prediction = lm.predict(data)[0]
+
+        return render_template('result.html', prediction=prediction)
+
+    return render_template('predict.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
